7772ef4876ebef79711e468152c0df7184a7e678
Jose Vargas <josedvq@gmail.com>
2020-12-13 00:10:02 +0100

    updating settings

--- covfee.production.config.py
+++ covfee.production.config.py
@@ -5,7 +5,8 @@ API_URL = APP_URL + '/api'
 APP_PORT = 80
 
 STATIC_URL = 'http://helix.ewi.tudelft.nl/covfee-static'
-BUNDLE_URL = STATIC_URL + '/bundle.js'
+BUNDLE_URL = 'http://helix.ewi.tudelft.nl/lared-www/main.js'
+ADMIN_BUNDLE_URL = 'http://helix.ewi.tudelft.nl/lared-www/admin.js'
 
-MEDIA_URL = 'http://helix.ewi.tudelft.nl/lared-media'
+MEDIA_URL = 'http://helix.ewi.tudelft.nl/lared-www'
 MEDIA_SERVER = False
